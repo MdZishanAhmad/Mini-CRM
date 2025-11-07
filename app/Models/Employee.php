52385e0a0c19c27b7e3f2b71390a4e74902db780
MdZishanAhmad <zishanahmad725@gmail.com>
2025-11-07 20:53:57 +0545

    Create Company and Employee models with relationships

---
+++ app/Models/Employee.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'company_id',
+        'email',
+        'phone',
+    ];
+
+    public function company():BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+    public function getFullNameAttribute():string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+}
