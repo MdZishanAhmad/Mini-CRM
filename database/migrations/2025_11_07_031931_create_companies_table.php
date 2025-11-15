870ecd4082a064d17cc73069ba51cdf915529654
MdZishanAhmad <zishanahmad725@gmail.com>
2025-11-15 09:40:20 +0545

    Create Companies and employees migration

--- database/migrations/2025_11_07_031931_create_companies_table.php
+++ database/migrations/2025_11_07_031931_create_companies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->nullable();
-            $table->string('logo')->unique();
+            $table->string('logo')->nullable();
             $table->string('website')->nullable();
             $table->timestamps();
         });
